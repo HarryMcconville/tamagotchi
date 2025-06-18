b50f5ef3f8c6bf386614017d1f80d4858ef566e8
Maddy Miller <maddyjmiller@outlook.com>
2025-06-18 15:21:48 +0100

    merged main into this

--- src/main/java/com/makers/tamagotchi/Controller/WelcomeController.java
+++ src/main/java/com/makers/tamagotchi/Controller/WelcomeController.java
@@ -1,6 +1,6 @@
 package com.makers.tamagotchi.Controller;
 
-import com.github.javafaker.Faker;
+
 import com.makers.tamagotchi.Model.User;
 import com.makers.tamagotchi.Model.Pet;
 import com.makers.tamagotchi.Repository.UserRepository;
